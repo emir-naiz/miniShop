119e5b505bb9da1910f6577887975403cce35ecf
emir-naiz <naizabekoff@mail.ru>
2021-03-05 20:31:57 +0600

    hidden EMAIL_HOST, EMAIL_HOST_PASSWORD

--- miniMarket/Mini-Shop/settings.py
+++ miniMarket/Mini-Shop/settings.py
@@ -129,6 +129,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bezman.shop@gmail.com'
-EMAIL_HOST_PASSWORD = 'pythonmorning123'
+EMAIL_HOST_USER = '/'
+EMAIL_HOST_PASSWORD = '/'
 EMAIL_PORT = 587
